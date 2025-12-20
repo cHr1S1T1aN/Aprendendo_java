02797ccd2877b24471cc751d1a52236eb9926651
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-12-20 17:02:21 -0300

    aprendendo arrays em java

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class Main{
 	static Scanner sc = new Scanner(System.in);
 	static int contador(){
-		int num1;
+		int num1=0;
+		int [] numeros = new int[5];
 		int soma = 0;
-		System.out.println("digite um numero para somar ou digite 0 para parar: ");
-		num1= sc.nextInt();
-		while(num1 != 0){	
-			soma = soma+num1;
-		System.out.println("digite um numero para somar ou digite 0 para parar: ");
-		num1= sc.nextInt();
+
+		for(num1=num1;num1 < 5;num1++){
+		System.out.println("digite 5 numeros: ");
+		numeros[num1] = sc.nextInt();	
+		soma += numeros[num1];
 		}
 		return soma;		
 	}
