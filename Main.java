6826382cabdcc8e886f3ce23a93e410ac3f32c45
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-12-22 17:33:04 -0300

    aula 3 de java

--- Main.java
+++ Main.java
@@ -1,21 +1,50 @@
 import java.util.Scanner;
-public class Main{
-	static Scanner sc = new Scanner(System.in);
-	static int contador(){
-		int num1=0;
-		int [] numeros = new int[5];
-		int soma = 0;
 
-		for(num1=num1;num1 < 5;num1++){
-		System.out.println("digite 5 numeros: ");
-		numeros[num1] = sc.nextInt();	
-		soma += numeros[num1];
-		}
-		return soma;		
-	}
-	public static void main(String[]args) {
-		int resultado = contador();
-        	System.out.println("Resultado: " + resultado);
-       		sc.close();
-	}	
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    static int[] lerNumeros(int qtd) {
+        int[] nums = new int[qtd];
+
+        for (int i = 0; i < qtd; i++) {
+            System.out.print("Digite o numero " + (i + 1) + ": ");
+            nums[i] = sc.nextInt();
+        }
+
+        return nums;
+    }
+
+    static int calcularSoma(int[] nums) {
+        int soma = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            soma += nums[i];
+        }
+
+        return soma;
+    }
+
+    static double calcularMedia(int soma, int qtd) {
+        return (double) soma / qtd;
+    }
+
+    static void resultado(int soma, double media) {
+        System.out.println("Soma: " + soma);
+        System.out.println("Media: " + media);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.print("Quantos numeros voce quer digitar? ");
+        int qtd = sc.nextInt();
+
+        int[] nums = lerNumeros(qtd);
+        int soma = calcularSoma(nums);
+        double media = calcularMedia(soma, qtd);
+
+        resultado(soma, media);
+
+        sc.close();
+    }
 }
