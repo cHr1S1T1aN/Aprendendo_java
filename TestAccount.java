52ab8afafcd52ca34718faf592773198647f38fe
cHr1S1T1aN <chrismorats50918@gmail.com>
2026-01-06 14:37:41 -0300

    aula dia 06/01

---
+++ TestAccount.java
@@ -0,0 +1,18 @@
+public class TestAccount {
+
+    private double balance;
+
+    public Account(double initialBalance) {
+        if (initialBalance > 0.0) {
+            balance = initialBalance;
+        }
+    }
+
+    public void credit(double amount) {
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
