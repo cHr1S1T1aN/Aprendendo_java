214e7ba548cf8e2ada52a1a705eae53437523908
cHr1S1T1aN <chrismorats50918@gmail.com>
2026-02-12 15:58:32 -0300

    aula dia 12/02/2025

---
+++ MaximumFinder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class MaximumFinder
+{
+	public void determineMaximum()
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter three fl oatingpoint values separated by spaces: " );
+		
+		double number1 = input.nextDouble();
+		double number2 = input.nextDouble();		
+		double number3 = input.nextDouble();
+
+		double result = maximum(number1,number2,number3)
+		System.out.println( "Maximum is: " + result )
+	}
+	
+	public double maximum(double x,double y,double z)
+	{
+		double maximumValue = x;
+		
+		if (y > maximumValue)
+		{	
+			maximumValue = y;
+		elif (z > maximumValue)
+		{
+			maximumValue = z;
+		return maximumValue;
+	}
+}
+		
---
+++ maximumFinderTest.java
@@ -0,0 +1,8 @@
+public class maximumFinderTest
+{
+	public static void main(String [] args)
+	{
+		MaximumFinder maximumFinder = new MaximumFinder();
+		maximumFinder.determineMaximum();
+	}
+}
